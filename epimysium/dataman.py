3ae19e7f979f19f43838a737f9702d52b5d8d51a
Christopher Dembia <cld72@cornell.edu>
2013-11-29 17:18:52 -0800

    Make a TRCFile class.

--- epimysium/dataman.py
+++ epimysium/dataman.py
@@ -43,6 +43,171 @@ if sys.version_info[0] == 2 and sys.version_info[1] < 6:
         return os.path.join(*rel_list)
     os.path.relpath = relpath
 
+class TRCFile(object):
+    """A plain-text file format for storing motion capture marker trajectories.
+    TRC stands for Track Row Column.
+
+    See
+    http://simtk-confluence.stanford.edu:8080/display/OpenSim/Marker+(.trc)+Files
+    for more information.
+
+    """
+    def __init__(self, fpath):
+        """
+        Parameters
+        ----------
+        fpath : str
+            Valid file path to a TRC (.trc) file.
+
+        """
+        # Read the header lines / metadata.
+        # ---------------------------------
+        # Split by any whitespace.
+        # TODO may cause issues with paths that have spaces in them.
+        f = open(fpath)
+        # These are lists of each entry on the first few lines.
+        first_line = f.readline().split()
+        # Skip the 2nd line.
+        f.readline()
+        third_line = f.readline().split()
+        fourth_line = f.readline().split()
+        f.close()
+
+        # First line.
+        self.path = first_line[3]
+
+        # Third line.
+        self.data_rate = float(third_line[0])
+        self.camera_rate = float(third_line[1])
+        self.num_frames = int(third_line[2])
+        self.num_markers = int(third_line[3])
+        self.units = third_line[4]
+        self.orig_data_rate = float(third_line[5])
+        self.orig_data_start_frame = int(third_line[6])
+        self.orig_num_frames = int(third_line[7])
+
+        # Marker names.
+        # The first and second column names are 'Frame#' and 'Time'.
+        self.marker_names = fourth_line[2:]
+
+        len_marker_names = len(self.marker_names)
+        if len_marker_names != self.num_markers:
+            warnings.warn('Header entry NumMarkers, %i, does not '
+                    'match actual number of markers, %i. Changing '
+                    'NumMarkers to match actual number.' % (
+                        self.num_markers, len_marker_names))
+            self.num_markers = len_marker_names
+
+        # Load the actual data.
+        # ---------------------
+        col_names = ['frame_num', 'time']
+        # This naming convention comes from OpenSim's Inverse Kinematics tool,
+        # when it writes model marker locations.
+        for mark in self.marker_names:
+            col_names += [mark + '_tx', mark + '_ty', mark + '_tz']
+        dtype = {'names': col_names,
+                'formats': ['int'] + ['float64'] * (3 * self.num_markers + 1)}
+        self.data = np.loadtxt(fpath, delimiter='\t', skiprows=6, dtype=dtype)
+        self.time = self.data['time']
+
+        # Check the number of rows.
+        n_rows = self.time.shape[0]
+        if n_rows != self.num_frames:
+            warnings.warn('Header entry NumFrames, %i, does not '
+                    'match actual number of frames, %i, Changing '
+                    'NumFrames to match actual number.' % (
+                        self.num_frames, n_rows))
+            self.num_frames = n_rows
+
+    def __getitem__(self, key):
+        """See `marker()`.
+
+        """
+        return self.marker(key)
+
+    def marker(self, name):
+        """The trajectory of marker `name`, given as a `self.num_frames` x 3
+        array. The order of the columns is x, y, z.
+
+        """
+        this_dat = np.empty((self.num_frames, 3))
+        this_dat[:, 0] = self.data[name + '_tx']
+        this_dat[:, 1] = self.data[name + '_ty']
+        this_dat[:, 2] = self.data[name + '_tz']
+        return this_dat
+
+    def add_marker(self, name, x, y, z):
+        """Add a marker, with name, `name`, to the TRCFile.
+
+        """
+        if (len(x) != self.num_frames or len(y) != self.num_frames or len(z) !=
+                self.num_frames):
+            raise Exception('Length of data (%i, %i, %i) is not '
+                    'NumFrames (%i).', len(x), len(y), len(z), self.num_frames)
+        self.marker_names += [name]
+        self.num_markers += 1
+        self.data[name] = np.concatenate(x, y, z, axis=1)
+
+    def marker_exists(self, name):
+        """
+        Returns
+        -------
+        exists : bool
+
+        """
+        return name in self.marker_names
+
+    def write(self, fpath):
+        """Write this TRCFile object to a TRC file.
+
+        Parameters
+        ----------
+        fpath : str
+            Valid file path to which this TRCFile is saved.
+
+        """
+        f = open(fpath, 'w')
+
+        # Line 1.
+        f.write('PathFileType  4\t(X/Y/Z) %s\n' % os.path.split(fpath)[0])
+
+        # Line 2.
+        f.write('DataRate\tCameraRate\NumFrames\tNumMarkers\t'
+                'Units\tOrigDataRate\tOrigDataStartFrame\tOrigNumFrames\n')
+
+        # Line 3.
+        f.write('%.1f\t%.1f\t%i\t%i\t%s\t%.1f\t%i\t%i\n' % (
+            self.data_rate, self.camera_rate, self.num_frames,
+            self.num_markers, self.units, self.orig_data_rate,
+            self.orig_data_start_frame, self.orig_num_frames))
+
+        # Line 4.
+        f.write('Frame#\tTime\t')
+        for imark in self.num_markers:
+            f.write('%s\t\t\t' % self.marker_names[imark])
+        f.write('\n')
+
+        # Line 5.
+        f.write('\t\t')
+        for imark in self.num_markers:
+            f.write('X%i\tY%s\tZ%s\t' % (imark, imark, imark))
+        f.write('\n')
+
+        # Line 6.
+        f.write('\n')
+
+        # Data.
+        for iframe in self.num_frames:
+            f.write('%i' % iframe)
+            f.write('\t%.5f', self.time[iframe])
+            for mark in self.marker_names:
+                idxs = [mark + '_tx', mark + '_ty', mark + '_tz']
+                f.write('\t%.3f\t%.3f\t%.3f' % (self.data[idxs]))
+            f.write('\n')
+
+        f.close()
+
+
 def ndarray2storage(ndarray, storage_fpath, name=None, in_degrees=False):
     """Saves an ndarray, with named dtypes, to an OpenSim Storage file.
 
